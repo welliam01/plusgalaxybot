262ff38b100d420982301a59798daebb1d0ce6ad
yousseftange17 <46229103+yousseftange17@users.noreply.github.com>
2019-02-14 23:54:58 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['536509623734370304'];
 const db = require('quick.db');
 const premium = ['470896018603376640']
 const client = new Discord.Client();   
